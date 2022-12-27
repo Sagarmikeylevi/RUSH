8aa5a2fb8b4d864f1234b40409c86cc9bc7de352
unknown <sagarmikeylevi@gmail.com>
2022-12-27 12:34:50 +0530

    Create Post and commnets

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,17 @@
+const { populate } = require('../models/comment');
 const Post = require('../models/post');
 
 module.exports.home = function(req , res){
     // populate the whole user object
-    Post.find({}).populate('user').exec(function(err , posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate:{
+            path: 'user'
+        }
+    })
+    .exec(function(err , posts){
         return res.render('home' , {
             title: "RUSH | Home",
             posts: posts
